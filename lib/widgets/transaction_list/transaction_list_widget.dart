94805dc4400b0bb38f2a1f1eca8546513fc356cd
bladerangel <bladerangel10@gmail.com>
2021-01-05 19:05:36 -0300

    Add image when transactions are empty

--- lib/widgets/transaction_list/transaction_list_widget.dart
+++ lib/widgets/transaction_list/transaction_list_widget.dart
@@ -13,34 +13,53 @@ class TransactionListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (context, index) => Card(
-          child: Row(
-            children: [
-              Style.amountContainer(
-                context: context,
-                child: Style.amountText(
-                  '\$${transactions[index].amout.toStringAsFixed(2)}',
-                  context: context,
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No transaction',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (context, index) => Card(
+                child: Row(
+                  children: [
+                    Style.amountContainer(
+                      context: context,
+                      child: Style.amountText(
+                        '\$${transactions[index].amout.toStringAsFixed(2)}',
+                        context: context,
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Style.titleText(
+                          transactions[index].title,
+                          context: context,
+                        ),
+                        Style.dateText(
+                          DateFormat.yMMMMd().format(transactions[index].date),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Style.titleText(
-                    transactions[index].title,
-                    context: context,
-                  ),
-                  Style.dateText(
-                    DateFormat.yMMMMd().format(transactions[index].date),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
