202af52a471865b86b86fa0b6df495d8401c70ae
bladerangel <bladerangel10@gmail.com>
2021-01-05 21:12:11 -0300

    Change transactions group order

--- lib/widgets/chart_bar/chart_bar_widget.dart
+++ lib/widgets/chart_bar/chart_bar_widget.dart
@@ -16,9 +16,12 @@ class ChartBarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(
-          child: Text(
-            '\$${amount.toStringAsFixed(0)}',
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text(
+              '\$${amount.toStringAsFixed(0)}',
+            ),
           ),
         ),
         SizedBox(
